63c34f241dbb01de9ceac037dbd2f0d440ff5f15
Yana Mishina <Lion_best85@list.ru>
2021-08-11 02:56:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int oneMile = 20;
+
+        int bonusMiles = ticketPrice / oneMile;
+
+        System.out.println("Начисленные бонусные мили: " + bonusMiles);
+    }
+}
